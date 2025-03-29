2320f9c2ef076dcab6245884acec6eff4dc984b6
Unicomfacauca <unicomfacauca@PPN-MAC-PC06.local>
2025-03-29 16:53:20 -0500

    cerror solucion

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 import pickle
 import pandas as pd
 from pydantic import BaseModel
-
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 # Cargar los modelos
 with open('RFCropRecommendation.pkl', 'rb') as f:
@@ -11,10 +12,17 @@ with open('RFCropRecommendation.pkl', 'rb') as f:
 with open('SVMModel.pkl', 'rb') as f:
     svm_model = pickle.load(f)
 
-
-
 # Crear aplicación FastAPI
 app = FastAPI()
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir acceso desde cualquier origen
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos HTTP (POST, GET, OPTIONS, etc.)
+    allow_headers=["*"],  # Permitir todos los encabezados HTTP
+)
 
 # Definir el esquema de entrada usando Pydantic
 class CropInput(BaseModel):
@@ -25,6 +33,11 @@ class CropInput(BaseModel):
     humidity: float
     ph: float
     rainfall: float
+
+# Ruta para manejar las solicitudes OPTIONS manualmente
+@app.options("/predict-rf/")
+async def handle_options():
+    return JSONResponse(status_code=200, content={"message": "CORS preflight successful"})
 
 # Definir el endpoint para el modelo RandomForest
 @app.post("/predict-rf/")
@@ -41,8 +54,6 @@ def predict_svm(input_data: CropInput):
     input_df = pd.DataFrame([input_dict])
     prediction = svm_model.predict(input_df)
     return {"prediction": prediction[0]}
-
-
 
 # Ruta de bienvenida
 @app.get("/")
